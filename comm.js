3694f8bab5199912e48a2d6f83f72cf44e0c30fc
Bahgat Ayman <126362104+bahgatayman@users.noreply.github.com>
2025-09-14 10:36:43 +0300

    Update comm.js

--- comm.js
+++ comm.js
@@ -1,15 +1,12 @@
 function showPopup(popupId) {
-    // إغلاق جميع النوافذ المفتوحة
     const popups = document.querySelectorAll('.popup');
     popups.forEach(popup => {
         popup.style.display = 'none';
     });
 
-    // فتح النافذة المطلوبة
     const popup = document.getElementById(popupId);
     popup.style.display = 'flex';
 
-    // إضافة حدث الإغلاق عند الضغط خارج المحتوى
     popup.addEventListener('click', function (event) {
         if (event.target === popup) {
             closePopup(popupId);
@@ -20,3 +17,4 @@ function showPopup(popupId) {
 function closePopup(popupId) {
     document.getElementById(popupId).style.display = 'none';
 }
+
